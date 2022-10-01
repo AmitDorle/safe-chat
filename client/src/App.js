183329dd39a44e502348344b1e53bc9df3a57610
Amit T. Dorle <Amitdorle111@gmail.com>
2022-10-01 11:00:09 +0530

    Login Page Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,24 @@
 import './App.css';
-import {useState} from 'react'
-import axios from 'axios';
+import Home from './Home/Home';
+import Login from './Login/Login';
+import Signup from './Signup/Signup';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
-
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  async function addUser()
-  {
-    console.log("FullName", fullName);
-    console.log("Email", email);
-    console.log("Password", password);
-
-    await axios.post('/signup', {
-      fullName: fullName,
-      email: email,
-      password: password})
-
-    setFullName("");
-    setEmail("");
-    setPassword("");
-
-    alert('User added successfully!')
-  }
-
   return (
     <div className="container">
-      <h1 className="text-center">ChatApp</h1>
-        <div className="row">
-          <div className="col-md-6">
-            <div className="my-form">
-              <h2 className="text-center">Signup</h2>
-
-              <div class="mb-3">
-                <label for="inputFullName" class="form-label">Full Name</label>
-                <input type="text" class="form-control" id="inputFullName" placeholder="Enter Your Name"
-                  value={fullName} onChange={(e)=>{setFullName(e.target.value)}} />
-              </div>
-
-              <div class="mb-3">
-                <label for="inputEmail" class="form-label">Email</label>
-                <input type="email" class="form-control" id="inputEmail" placeholder="Enter Your Email"
-                 value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
-              </div>
-
-              <div class="mb-3">
-                <label for="inputPassword" class="form-label">Password</label>
-                <input type="password" class="form-control" id="inputPassword" placeholder="Enter Your Password"
-                  value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
-              </div>
-
-              <button type="button" className="btn btn-warning w-100" onClick={addUser}>Create An Account</button>
-            </div>
-          </div>
-          <div className="col-md-6">
-
-          </div>
-        </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<Signup />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/Login/Login.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import {useState} from 'react'
+import axios from 'axios';
+
+import './Login.css'
+function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function loginUser()
+  {
+
+    const response = await axios.post('/login', {
+      email: email,
+      password: password})
+
+      if(response.data.success === true)
+      {
+        alert("Logged in successful")
+        const user = response.data.data;
+        localStorage.setItem('user',JSON.stringify(user))
+      }
+      else
+      {
+        alert("login failed")
+      }
+
+    setEmail("");
+    setPassword("");
+  }
+
+  return (
+    <div>
+    <div className="row">
+    <div className="col-md-12">
+    <div className="my-form">
+        <h2 className="text-center">Sign In</h2>
+
+        <div class="mb-3">
+          <label for="inputEmail" class="form-label">Email</label>
+          <input type="email" class="form-control" id="inputEmail" placeholder="Enter Your Email"
+           value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+        </div>
+
+        <div class="mb-3">
+          <label for="inputPassword" class="form-label">Password</label>
+          <input type="password" class="form-control" id="inputPassword" placeholder="Enter Your Password"
+            value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+        </div>
+
+        <button type="button" className="btn btn-warning w-100" onClick={loginUser}>Login</button>
+      </div>
+    </div>
+  </div>
+  </div>
+  )
+}
+
+export default Login
